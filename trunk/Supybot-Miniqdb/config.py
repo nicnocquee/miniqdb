77d06dc29d4bfefca9fe30889c705ca39a419a83
ianweller <ianweller@0559f54a-e046-0410-9701-eb5f1cc0e294>
2008-03-30 06:31:30 +0000

    checking in Miniqdb supybot plugin

---
+++ trunk/Supybot-Miniqdb/config.py
@@ -0,0 +1,60 @@
+###
+# miniqdb - A simple quote database written in PHP
+# Copyright (C) 2008  Ian Weller <ianweller@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+###
+
+import supybot.conf as conf
+import supybot.registry as registry
+
+def configure(advanced):
+    # This will be called by supybot to configure this module.  advanced is
+    # a bool that specifies whether the user identified himself as an advanced
+    # user or not.  You should effect your configuration by manipulating the
+    # registry as appropriate.
+    from supybot.questions import expect, anything, something, yn
+    output("""Where is your miniqdb instance located? Please don't put a slash
+    at the end of the URL -- for example, http://example.com/miniqdb .""")
+    root = something('root?')
+    conf.registerPlugin('Miniqdb', True)
+    conf.supybot.plugins.Miniqdb.miniqdbRoot.setValue(root)
+    output("""This plugin has support to utilize HTTP Basic authentication.
+    HTTP Digest is not supported just yet.""")
+    if yn("Do you use HTTP Basic authentication on your miniqdb?"):
+        conf.supybot.plugins.Miniqdb.auth.username.setValue(something('username?'))
+        conf.supybot.plugins.Miniqdb.auth.username.setValue(something('password?'))
+    output("""Thanks for using miniqdb! http://miniqdb.googlecode.com/""")
+
+
+Miniqdb = conf.registerPlugin('Miniqdb')
+# This is where your configuration variables (if any) should go.  For example:
+# conf.registerGlobalValue(Miniqdb, 'someConfigVariableName',
+#     registry.Boolean(False, """Help for someConfigVariableName."""))
+conf.registerChannelValue(Miniqdb, 'miniqdbRoot', registry.String('', """The
+    root of the miniqdb directory. For example,
+    http://example.com/miniqdb -- don't put a slash at the end."""))
+conf.registerChannelValue(Miniqdb, 'maxLines', registry.PositiveInteger(3,
+    """The maximum number of lines that will be printed to a channel. If a
+    quote is longer than this, a link and the number of lines will be printed
+    to the channel. Set to 0 to disable quote printing."""))
+conf.registerGroup(Miniqdb, 'auth')
+conf.registerChannelValue(Miniqdb.auth, 'username', registry.String("", """The
+    username for HTTP authentication."""))
+conf.registerChannelValue(Miniqdb.auth, 'password', registry.String("", """The
+    password for HTTP authentication."""))
+
+
+# vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
---
+++ trunk/Supybot-Miniqdb/plugin.py
@@ -0,0 +1,87 @@
+###
+# miniqdb - A simple quote database written in PHP
+# Copyright (C) 2008  Ian Weller <ianweller@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+###
+
+import supybot.utils as utils
+import supybot.conf as conf
+from supybot.commands import *
+import supybot.plugins as plugins
+import supybot.ircutils as ircutils
+import supybot.callbacks as callbacks
+import urllib
+import httplib
+import urlparse
+import re
+from xml.dom import minidom 
+
+
+class Miniqdb(callbacks.Plugin):
+    """Use this plugin in conjunction with a miniqdb instance on a server. You
+    can use the "quote" command to view the link and information about a
+    quote."""
+    threaded = True
+
+    def _prompt_user_passwd(self, host, realm):
+        username = conf.supybot.plugins.Miniqdb.auth.username()
+        password = conf.supybot.plugins.Miniqdb.auth.password()
+        return (username, password)
+    urllib.FancyURLopener.prompt_user_passwd = _prompt_user_passwd
+
+    def quote(self, irc, msg, args, id):
+        """<id>
+
+        Returns a link to and information about quote <id>."""
+        root = conf.supybot.plugins.Miniqdb.miniqdbRoot()
+        maxlines = conf.supybot.plugins.Miniqdb.maxLines()
+        url = str(root) + '/api.php?method=rest&act=quote&id=' +str(id)
+        opener = urllib.FancyURLopener()
+        xml = opener.open(url).read()
+        dom = minidom.parseString(xml)
+        errors = dom.getElementsByTagName('miniqdb')[0].getElementsByTagName('error')
+        if errors != []:
+            reply = "There is no quote by that id."
+        else:
+            quote = dom.getElementsByTagName('miniqdb')[0].getElementsByTagName('quote')[0]
+            lines = int(quote.getAttribute('lines'))
+            if lines > maxlines:
+                reply = root+'/quote.php?id='+str(id) + ', ' + str(lines) + ' lines'
+            else:
+                reply = quote.firstChild.data.replace('&lt;','<').replace('&gt;','>')
+        for line in reply.split('\n'):
+            irc.reply(line)
+    quote = wrap(quote, ['id'])
+
+    def stats(self, irc, msg, args):
+        """takes no arguments
+
+        Returns some statistics from the QDB."""
+        root = conf.supybot.plugins.Miniqdb.miniqdbRoot()
+        maxlines = conf.supybot.plugins.Miniqdb.maxLines()
+        url = str(root) + '/api.php?method=rest&act=stats'
+        opener = urllib.FancyURLopener()
+        xml = opener.open(url).read()
+        dom = minidom.parseString(xml)
+        stats = dom.getElementsByTagName('miniqdb')[0].getElementsByTagName('stats')[0]
+        count = stats.getAttribute('count')
+        irc.reply(format("There are %s quotes in the database.", count))
+
+
+Class = Miniqdb
+
+
+# vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
